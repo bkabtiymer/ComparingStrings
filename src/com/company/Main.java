099fc22ef259b32675ce93d33ad7fe18b5c14e80
bkabtiymer <bkabtiymer@gmail.com>
2019-04-17 10:12:43 -0400

    Hello guys!

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double Inches_to_gallons = 231;
+
+        Integer age = 99;
+        String StrAge = age.toString();
+
+
+        System.out.print("Enter your name: ");
+        Scanner keyboard = new Scanner(System.in);
+        String name=keyboard.nextLine();
+
+
+        System.out.println("Hello"+ name + "!");
+        System.out.println("Today is April 17,2019. I am alive!!");
+
+
+
+
+
+
+}
+}
